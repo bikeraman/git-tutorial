78f2a67d83c1ff8059355d65176811f99e8937b2
bikeraman <amansahni2000@gmail.com>
2023-04-29 16:59:28 +0530

    adding name

---
+++ aman.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	char* string = "Aman Sahni."; // Your name here. ^_^
+	char* string1 = " - Aman Sahni"; // Your name here. ^_^
+	char* string = "Aman Sahni"; // Your name here. ^_^
+	char* string = "Aman Sahni."; // Your name here. ^_^
+	char* string = "Aman Sahni."; // Your name here. ^_^
+  char* string = "Aman Sahni."; // Your name here. ^_^
+	char string = "Aman Sahni"; //Your name here. ^_^
+	printf("%s\n", string);
+	printf("just to see the conflict :) %s\n",string1);
+	return 0;
+}
